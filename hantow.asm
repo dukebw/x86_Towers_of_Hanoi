3fb3db05f5b0847506d926ca2344bb6ba5ac56d3
Brendan Duke <dukebw@mcmaster.ca>
2014-11-27 14:46:00 -0500

    Added display_array function

--- hantow.asm
+++ hantow.asm
@@ -9,19 +9,43 @@
 
 SECTION .data
   ; Initialize the three pegs to be empty.
-  Peg1 db 0,0,0,0,0,0,0,0,9
-  Peg2 db 0,0,0,0,0,0,0,0,9
-  Peg3 db 0,0,0,0,0,0,0,0,9
+  Peg1: dd 0,0,0,0,0,0,0,0,9
+  Peg2: dd 0,0,0,0,0,0,0,0,9
+  Peg3: dd 0,0,0,0,0,0,0,0,9
+  Peg_len equ $-Peg3
 
 SECTION .bss
 
 SECTION .text
    global asm_main
 
-asm_main:
-   enter 0,0    ; enter the subroutine 
-   pushad       ; save all registers
+;-----------------------------------------------------------------------------
+; Prints the array currently addressed by eax, using eax as a pointer to each
+; element.
+Display_array:
+      push ebx      ; We will use ebx, so we save it.
+      push esi      ; We will use esi, so we save it.
+      mov esi,0     ; esi will index the array so we set it to zero.
+      mov ebx,eax   ; Put base address of array in eax.
+.Loop:
+      mov eax,dword [ebx+esi] ; Put current element of array in eax. 
+      call print_int ; Print element.
+      mov eax,' '   ; Put format character ' ' in eax.
+      call print_char ; Print space.
+      add esi,4     ; eax+esi points to next int in array.
+      cmp esi,Peg_len ; Check if pointer points to end of array
+      jl .Loop      ; If not, print next number.
+      call print_nl ; Print a formatting newline.
+      mov eax,ebx   ; Restore base of array to eax.
+      pop esi       ; Restore registers and return.
+      pop ebx
+      ret
 
-   popad        ; restore all registers
-   leave        ; leave the subroutine
-   ret          ; return control
+asm_main:
+      enter 0,0     ; Enter the subroutine.
+      pushad        ; Save all registers.
+      mov eax,Peg1  ; Test printing Peg1
+      call Display_array ; Call print array function.
+      popad         ; Restore all registers.
+      leave         ; Leave the subroutine.
+      ret           ; Return control.
--- makefile
+++ makefile
@@ -1,3 +1,18 @@
 # Makefile for Towers of Hanoi x86 assembly project
 # Author: Brendan Duke
 # Modified: November 25, 2014
+sandbox: sandbox.o
+	ld -o sandbox sandbox.o
+sandbox.o: sandbox.asm
+	nasm -f elf64 -g -F stabs sandbox.asm -l sandbox.lst
+
+asm_io.o: asm_io.asm
+	nasm -f elf32 -d ELF_TYPE asm_io.asm
+
+hantow: hantow.o asm_io.o
+	gcc -m32 -o hantow hantow.o driver.c asm_io.o
+hantow.o: hantow.asm
+	nasm -f elf32 -g -F stabs hantow.asm
+
+clean:
+	rm -f *.o *~ sandbox hantow
